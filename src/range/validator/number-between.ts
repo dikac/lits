5c8be842e57d5556576e32c2e29dc4ad9472f620
dikac <rigel234@yahoo.com>
2019-08-28 18:20:16 +0700

    add number between

---
+++ src/range/validator/number-between.ts
@@ -0,0 +1,21 @@
+import Range  from '../range';
+import Validator  from '../../validator/validator';
+import Validatable  from '../../validatable/validatable';
+import Primitive  from '../../validatable/primitive';
+
+export default class implements Range, Validator<[number]> {
+
+    constructor(
+        public minimum : number = 0,
+        public maximum : number = Infinity
+    ) {
+
+    }
+
+    validate(number : number): Validatable {
+
+        return new Primitive(number >= this.minimum && number <= this.maximum);
+
+    }
+
+}

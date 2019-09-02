91eba0ccdd733291f6be6fc6650d319293652a20
dikac <rigel234@yahoo.com>
2019-09-02 13:08:37 +0700

    disable and & or for the moment

--- src/validator/and.ts
+++ src/validator/and.ts
@@ -1,24 +1,24 @@
 import AbstractCompound from "./abstract-compound";
 import Validatable from "../validatable/validatable";
-
-
-export default class <Value, Return extends Validatable = Validatable> extends AbstractCompound <Value, Return> {
-
-    validate(value: Value): Return {
-
-        let boolean : boolean|null = null;
-
-        for(let boolean of this.validators) {
-
-            if(!(boolean = boolean.validate(value))) {
-
-                return false;
-            }
-        }
-
-        return true;
-
-        //return bool ? true : this.empty;
-    }
-}
+//
+//
+// export default class <Value, Return extends Validatable = Validatable> extends AbstractCompound <Value, Return> {
+//
+//     validate(value: Value): Return {
+//
+//         let boolean : boolean|null = null;
+//
+//         for(let boolean of this.validators) {
+//
+//             if(!(boolean = boolean.validate(value))) {
+//
+//                 return false;
+//             }
+//         }
+//
+//         return true;
+//
+//         //return bool ? true : this.empty;
+//     }
+// }
 
--- src/validator/or.ts
+++ src/validator/or.ts
@@ -1,20 +1,21 @@
-import Empty from './empty';
-
-export default abstract class <Item> extends Empty {
-
-    validate(booleans: Iterable<boolean>): boolean {
-
-        let bool : boolean|null = null;
-
-        for(bool of booleans) {
-
-            if(bool.valueOf()) {
-
-                return true;
-            }
-        }
-
-        return bool ? false : this.empty;
-    }
-}
+import Empty from './abstract-compound';
+import Validatable from "../validatable/validatable";
+//
+// export default abstract class <Value, Return extends Validatable> extends Empty<Value, Return> {
+//
+//     validate(booleans: Iterable<boolean>) : Return {
+//
+//         let bool : boolean|null = null;
+//
+//         for(bool of booleans) {
+//
+//             if(bool.valueOf()) {
+//
+//                 return true;
+//             }
+//         }
+//
+//         return bool ? false : this.empty;
+//     }
+// }
 

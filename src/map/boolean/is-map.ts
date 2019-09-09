ea512a5ef90fbbf337543ee6d9244bafb792b187
dikac <rigel234@yahoo.com>
2019-09-09 09:39:27 +0700

    add is map

---
+++ src/map/boolean/is-map.ts
@@ -0,0 +1,35 @@
+import HasMethods from "../../object/boolean/has-methods";
+import HasSymbolIterator from "../../object/boolean/has-symbol-iterator";
+import HasSymbolToString from "../../object/boolean/has-symbol-to-string";
+
+export default function<Key, Value> (value : any) : value is Map<Key, Value> {
+
+    if(typeof value !== 'object' || value === null) {
+
+        return false;
+    }
+
+        if(!('size' in value)) {
+
+            return false;
+        }
+
+        if(!HasMethods(value, ['clear', 'delete', 'forEach', 'get', 'has', 'set'])) {
+
+            return false;
+        }
+
+        if(!HasSymbolIterator(value)) {
+
+            return false;
+        }
+
+        if(!HasSymbolToString(value)) {
+
+            return false;
+        }
+
+        return true;
+}
+
+

0d1d2028a8077aafd1378bab8e8f3cb31792b35b
dikac <rigel234@yahoo.com>
2019-09-29 17:51:09 +0700

    update spec set 2

--- spec/set/wrapper.spec.ts
+++ spec/set/wrapper.spec.ts
@@ -5,12 +5,12 @@ it("force console log", () => { spyOn(console, 'log').and.callThrough();});
 describe("constructor", function() {
 
     let wrapper = new Wrapper<number>(new Set<number>([1,2,3,4]));
-    it("equal", () => expect([...wrapper]).toBe([1,2,3,4]));
+    it("equal", () => expect([...wrapper]).toEqual([1,2,3,4]));
 });
 
 describe("set", function() {
 
     let wrapper = new Wrapper<number>(new Set<number>());
     wrapper.add(1);
-    it("equal", () => expect([...wrapper]).toBe([1]));
+    it("equal", () => expect([...wrapper]).toEqual([1]));
 });

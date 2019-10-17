2ac419cf22cc1a0c25dfaa6594854f93770d776e
dikac <rigel234@yahoo.com>
2019-10-17 16:19:07 +0700

    add array functions

---
+++ src/array/number/remove-values.ts
@@ -0,0 +1,22 @@
+import Index from "../number";
+import RemoveIndex from "../boolean/remove-index";
+import RemoveValue from "../boolean/remove-value";
+
+export default function RemoveValues<Value = any>(
+    array : Value[],
+    values : Value[],
+    validator : (arrayValue : Value, valueArgument : Value) => boolean,
+    fromIndex : number = 0
+) : number {
+
+    let removed = 0;
+    for(let value of values) {
+
+        if(RemoveValue<Value>(array, value, validator, fromIndex)) {
+
+            removed++;
+        }
+    }
+
+    return removed;
+}
